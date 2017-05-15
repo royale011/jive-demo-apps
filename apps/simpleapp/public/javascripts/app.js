36d0ce4298fb2e2eda42effcfda6fa379780d704
Nero Lee <jxli@thoughtworks.com>
2017-05-15 17:56:05 +0800

    change dataUriToBlob

--- apps/simpleapp/public/javascripts/app.js
+++ apps/simpleapp/public/javascripts/app.js
@@ -156,28 +156,21 @@ function onData(data) {
  ######################################################################################## */
 
 function dataURItoBlob(dataURI) {
-    // convert base64 to raw binary data held in a string
-    // doesn't handle URLEncoded DataURIs - see SO answer #6850276 for code that does this
-    var byteString = atob(dataURI.split(',')[1]);
+    // convert base64/URLEncoded data component to raw binary data held in a string
+    var byteString;
+    if (dataURI.split(',')[0].indexOf('base64') >= 0)
+        byteString = atob(dataURI.split(',')[1]);
+    else
+        byteString = unescape(dataURI.split(',')[1]);
 
     // separate out the mime component
     var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
 
-    // write the bytes of the string to an ArrayBuffer
-    var ab = new ArrayBuffer(byteString.length);
-    var ia = new Uint8Array(ab);
+    // write the bytes of the string to a typed array
+    var ia = new Uint8Array(byteString.length);
     for (var i = 0; i < byteString.length; i++) {
         ia[i] = byteString.charCodeAt(i);
     }
 
-    //Old Code
-    //write the ArrayBuffer to a blob, and you're done
-    //var bb = new BlobBuilder();
-    //bb.append(ab);
-    //return bb.getBlob(mimeString);
-
-    //New Code
-    return new Blob([ab], {type: mimeString});
-
-
+    return new Blob([ia], {type:mimeString});
 }
