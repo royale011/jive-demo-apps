35b60f2eb1e4a247722615311b8acecdabcf5742
Nero Lee <jxli@thoughtworks.com>
2017-05-15 17:37:44 +0800

    use image blob

--- extension_src/public/apps/simpleapp/javascripts/app.js
+++ extension_src/public/apps/simpleapp/javascripts/app.js
@@ -101,11 +101,15 @@ function onViewer(viewer) {
         // }, function (error) {
         //     console.log('viewer error', error);
         // });
+        var imageBlob = dataURItoBlob(headerImage);
+        var formData = new FormData();
+        formData.append("file", imageBlob);
         osapi.jive.core.put({
             v: "v3",
-            href: "/people/" + '@viewer' + "/avatar?uri=" + headerImage
+            // userId: "@viewer",
+            href: "/people/@viewer/avatar",
+            body: formData
         }).execute(function (response) {
-            var id = response.id;
             console.log('response', response);
             container.hide();
             croppable = true;
@@ -148,3 +152,30 @@ function onData(data) {
  ###### AND ARE ONLY MEANT TO SHOW HOW TO PERFORM VARIOUS RTE INJECTIONS W/APPS #########
  ########################################################################################
  ######################################################################################## */
+
+function dataURItoBlob(dataURI) {
+    // convert base64 to raw binary data held in a string
+    // doesn't handle URLEncoded DataURIs - see SO answer #6850276 for code that does this
+    var byteString = atob(dataURI.split(',')[1]);
+
+    // separate out the mime component
+    var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+
+    // write the bytes of the string to an ArrayBuffer
+    var ab = new ArrayBuffer(byteString.length);
+    var ia = new Uint8Array(ab);
+    for (var i = 0; i < byteString.length; i++) {
+        ia[i] = byteString.charCodeAt(i);
+    }
+
+    //Old Code
+    //write the ArrayBuffer to a blob, and you're done
+    //var bb = new BlobBuilder();
+    //bb.append(ab);
+    //return bb.getBlob(mimeString);
+
+    //New Code
+    return new Blob([ab], {type: mimeString});
+
+
+}
